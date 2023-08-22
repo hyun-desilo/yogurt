d8a4dfb85b20179238744a97562585abcd45fb30
hyun <hyun.namgung@desilo.ai>
2023-08-22 21:20:18 +0900

    add test

---
+++ with-jest/vanilla/input.js
@@ -0,0 +1,13 @@
+// 안풋 요소 가져오기
+const input = document.getElementById("myInput");
+
+// input에 영어만 허용
+input.addEventListener('input', function(event) {
+  const inputValue = event.target.value;
+  const englishOnly = /^[A-Za-z]*$/;
+
+  if (!englishOnly.test(inputValue)) {
+    alert('영아만 입력 가능')
+    event.target.value = inputValue.replace(/[^A-Za-z]/g, '');
+  }
+});
