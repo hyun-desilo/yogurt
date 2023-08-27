c7aba345f80166678d8e5d592def4f36eaf64946
koreanthinker <koreanthinker@gmail.com>
2023-08-27 13:15:32 +0900

    add

--- with-jest/test.js
+++ with-jest/test.js
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom'
 import { JSDOM } from 'jsdom'
 import fs from 'fs'
 
-const html = fs.readFileSync('./vanilla/index.html', 'utf8');
+const html = fs.readFileSync('./src/index.html', 'utf8');
 
 it('renders a heading element', () => {
   const dom = new JSDOM(html, { runScripts: 'dangerously' })
